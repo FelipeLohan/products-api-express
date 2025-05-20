5d6fdedf4fc816486b0d853b6ae8b7a68ad18add
FelipeLohan <felipelohan03052006@gmail.com>
2025-05-20 12:07:59 -0300

    fix(models): version key false

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 
-const categorySchema = new mongoose.Schema({
-  id: {type: mongoose.Schema.Types.ObjectId},
-  name: { type: String, required: [true, "The name of category is required"]}
-})
+const categorySchema = new mongoose.Schema(
+  {
+    id: { type: mongoose.Schema.Types.ObjectId },
+    name: {
+      type: String,
+      required: [true, "The name of category is required"],
+    },
+  },
+  { versionKey: false }
+);
 
 const category = mongoose.model("categories", categorySchema);
 
--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,12 +1,22 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  id: { type: mongoose.Schema.Types.ObjectId },
-  name: { type: String, required: [true, "The name of product is required"]},
-  category: { type: mongoose.Schema.Types.ObjectId, ref: "categories", required: [true, "The category of product is required"] },
-  price: { type: Number , required: [true, "The price of product is required"]},
-  image: { type: String }
-});
+const productSchema = new mongoose.Schema(
+  {
+    id: { type: mongoose.Schema.Types.ObjectId },
+    name: { type: String, required: [true, "The name of product is required"] },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "categories",
+      required: [true, "The category of product is required"],
+    },
+    price: {
+      type: Number,
+      required: [true, "The price of product is required"],
+    },
+    image: { type: String },
+  },
+  { versionKey: false }
+);
 
 const product = mongoose.model("products", productSchema);
 
