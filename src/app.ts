024866239a3a3bef75425084698d4c6a3623262f
FelipeLohan <felipelohan03052006@gmail.com>
2025-05-20 10:44:37 -0300

    feat: database connection working

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,19 @@ import express from "express";
 import { Request, Response } from "express";
 import { DOMAIN, PORT } from "./utils/system.js";
 import "dotenv/config";
+import databaseConnection from "./config/databaseConection.js";
 
 const app = express();
+
+const conection = await databaseConnection();
+
+conection.on("error", (error) => {
+  console.log("Error:" + error);
+});
+
+conection.once("open", () => {
+  console.log("Connection Oppened");
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({"message": "Hello World!", "status": 200});
