67825005b4226c94ac5dea54e7c739a6b75efb6c
FelipeLohan <felipelohan03052006@gmail.com>
2025-05-20 10:10:10 -0300

    feat: testing routes

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
 import express from "express";
+import { Request, Response } from "express";
+import { DOMAIN, PORT } from "./utils/system.js";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({"message": "Hello World!", "status": 200});
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started in: ${DOMAIN}`);
+});
+
