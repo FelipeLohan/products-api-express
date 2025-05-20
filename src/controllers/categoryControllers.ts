a3fb83f8fddaeced2ecd4ab0c72e62d7b508b504
FelipeLohan <felipelohan03052006@gmail.com>
2025-05-20 18:43:37 -0300

    feat(controllers): added get and post category controllers

--- src/controllers/categoryControllers.ts
+++ src/controllers/categoryControllers.ts
@@ -1,1 +1,29 @@
+import { Request, Response } from "express";
+import category from "../models/Category.js";
 
+class CategoryController {
+  static findAllCategories = async (req: Request, res: Response) => {
+    try {
+      const categoriesData = await category.find();
+      res
+        .status(200)
+        .json({ "message": "success", "status": 200, "data": categoriesData });
+    } catch (e) {
+      res.status(500).json({ "message": "fail", "status": 500, "description": `${e}` });
+    }
+  };
+
+  static newCategory = async (req: Request, res: Response) => {
+    const newCategory = req.body;
+    try {
+      const newCategoryData = await category.create(newCategory);
+      res
+        .status(201)
+        .json({ "message": "success", "status": 201, "data": newCategoryData });
+    } catch (e) {
+      res.status(500).json({"message": "fail", "status": 500, "description": `${e}`})
+    }
+  };
+}
+
+export default CategoryController;
