eee7d8ca4c162dd2639e96f904a3764d430373ca
FelipeLohan <felipelohan03052006@gmail.com>
2025-05-21 11:53:54 -0300

    feat(routes): added findById product route

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -3,6 +3,7 @@ import ProductController from "../controllers/productControllers.js";
 
 const productsRoutes: Router = express.Router();
 
+productsRoutes.get("/v1/api/products/:id", ProductController.findById);
 productsRoutes.get("/v1/api/products", ProductController.findAllProducts);
 productsRoutes.post("/v1/api/products", ProductController.newProduct);
 
