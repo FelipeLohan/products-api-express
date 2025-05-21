ea775596cf54d7cab54f4803483756b2657fcf06
FelipeLohan <felipelohan03052006@gmail.com>
2025-05-21 12:10:59 -0300

    feat(routes): added updateProduct route

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -6,5 +6,6 @@ const productsRoutes: Router = express.Router();
 productsRoutes.get("/v1/api/products/:id", ProductController.findById);
 productsRoutes.get("/v1/api/products", ProductController.findAllProducts);
 productsRoutes.post("/v1/api/products", ProductController.newProduct);
+productsRoutes.put("/v1/api/products/:id", ProductController.updateProduct);
 
 export default productsRoutes;
