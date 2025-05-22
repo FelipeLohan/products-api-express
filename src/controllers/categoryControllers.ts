55a4bce154869c8090158daa2c3f0646c02ed32e
FelipeLohan <felipelohan03052006@gmail.com>
2025-05-22 07:43:07 -0300

    feat(controllers): added findByid category

--- src/controllers/categoryControllers.ts
+++ src/controllers/categoryControllers.ts
@@ -7,9 +7,26 @@ class CategoryController {
       const categoriesData = await category.find();
       res
         .status(200)
-        .json({ "message": "success", "status": 200, "data": categoriesData });
+        .json({ message: "success", status: 200, data: categoriesData });
     } catch (e) {
-      res.status(500).json({ "message": "fail", "status": 500, "description": `${e}` });
+      res
+        .status(500)
+        .json({ message: "fail", status: 500, description: `${e}` });
+    }
+  };
+
+  static findById = async (req: Request, res: Response) => {
+    const productId = req.params.id;
+
+    try {
+      const categoriesData = await category.findById(productId);
+      res
+        .status(200)
+        .json({ message: "success", status: 200, data: categoriesData });
+    } catch (e) {
+      res
+        .status(500)
+        .json({ message: "fail", status: 500, description: `${e}` });
     }
   };
 
@@ -19,9 +36,11 @@ class CategoryController {
       const newCategoryData = await category.create(newCategory);
       res
         .status(201)
-        .json({ "message": "success", "status": 201, "data": newCategoryData });
+        .json({ message: "success", status: 201, data: newCategoryData });
     } catch (e) {
-      res.status(500).json({"message": "fail", "status": 500, "description": `${e}`})
+      res
+        .status(500)
+        .json({ message: "fail", status: 500, description: `${e}` });
     }
   };
 }
