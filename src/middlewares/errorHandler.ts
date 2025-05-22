27657af16f1a334dc54641e4017526392a989fcc
FelipeLohan <felipelohan03052006@gmail.com>
2025-05-22 15:27:48 -0300

    feat(middlewares): added errorHandler middleware

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express';
+
+const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    message,
+    status: statusCode,
+    description: err.description || '',
+  });
+};
+
+export default errorHandler;
