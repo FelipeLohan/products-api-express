b42132282b230c465c6a3b9682ff94ed98da0be9
FelipeLohan <felipelohan03052006@gmail.com>
2025-05-22 16:00:22 -0300

    feat(routes): added validaObjectId in routes

--- src/routes/categoryRoutes.ts
+++ src/routes/categoryRoutes.ts
@@ -1,12 +1,13 @@
 import express, { Router } from "express";
 import CategoryController from "../controllers/categoryControllers.js";
+import { validateObjectId } from "../middlewares/validateObjectId.js";
 
 const categoriesRoutes: Router = express.Router();
 
-categoriesRoutes.get("/v1/api/categories/:id", CategoryController.findById);
+categoriesRoutes.get("/v1/api/categories/:id", validateObjectId ,CategoryController.findById);
 categoriesRoutes.get("/v1/api/categories", CategoryController.findAllCategories);
 categoriesRoutes.post("/v1/api/categories", CategoryController.newCategory);
-categoriesRoutes.put("/v1/api/categories/:id", CategoryController.updateCategory);
-categoriesRoutes.delete("/v1/api/categories/:id", CategoryController.deleteCategory);
+categoriesRoutes.put("/v1/api/categories/:id", validateObjectId , CategoryController.updateCategory);
+categoriesRoutes.delete("/v1/api/categories/:id", validateObjectId , CategoryController.deleteCategory);
 
 export default categoriesRoutes;
--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,12 +1,13 @@
 import express, { Router } from "express";
 import ProductController from "../controllers/productControllers.js";
+import { validateObjectId } from "../middlewares/validateObjectId.js";
 
 const productsRoutes: Router = express.Router();
 
-productsRoutes.get("/v1/api/products/:id", ProductController.findById);
+productsRoutes.get("/v1/api/products/:id", validateObjectId, ProductController.findById);
 productsRoutes.get("/v1/api/products", ProductController.findAllProducts);
 productsRoutes.post("/v1/api/products", ProductController.newProduct);
-productsRoutes.put("/v1/api/products/:id", ProductController.updateProduct);
-productsRoutes.delete("/v1/api/products/:id", ProductController.deleteProduct);
+productsRoutes.put("/v1/api/products/:id", validateObjectId, ProductController.updateProduct);
+productsRoutes.delete("/v1/api/products/:id", validateObjectId, ProductController.deleteProduct);
 
 export default productsRoutes;
