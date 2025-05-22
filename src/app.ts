e9f2d6c1cfd1029f4949bc020161347e0838298b
FelipeLohan <felipelohan03052006@gmail.com>
2025-05-22 15:29:00 -0300

    feat(app): added new errorHandler middleware using app.use

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { DOMAIN, PORT } from "./utils/system.js";
 import "dotenv/config";
 import databaseConnection from "./config/databaseConection.js";
 import routes from "./routes/index.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
 const app: Application = express();
 routes(app);
@@ -16,6 +17,8 @@ conection.on("error", (error) => {
 conection.once("open", () => {
   console.log("Connection Oppened");
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server started in: ${DOMAIN}`);
