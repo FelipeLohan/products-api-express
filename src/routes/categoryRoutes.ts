8682088bee041a0d36ca9ab4418487bd55f6f8e4
FelipeLohan <felipelohan03052006@gmail.com>
2025-05-22 08:11:39 -0300

    feat(routes): added deleteCategory route

--- src/routes/categoryRoutes.ts
+++ src/routes/categoryRoutes.ts
@@ -7,5 +7,6 @@ categoriesRoutes.get("/v1/api/categories/:id", CategoryController.findById);
 categoriesRoutes.get("/v1/api/categories", CategoryController.findAllCategories);
 categoriesRoutes.post("/v1/api/categories", CategoryController.newCategory);
 categoriesRoutes.put("/v1/api/categories/:id", CategoryController.updateCategory);
+categoriesRoutes.delete("/v1/api/categories/:id", CategoryController.deleteCategory);
 
 export default categoriesRoutes;
