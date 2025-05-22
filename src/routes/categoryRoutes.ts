ec144f188204e5e9b0cf0f0b559b8a3b8793f2ee
FelipeLohan <felipelohan03052006@gmail.com>
2025-05-22 07:43:43 -0300

    feat(routes): added findById category route

--- src/routes/categoryRoutes.ts
+++ src/routes/categoryRoutes.ts
@@ -3,6 +3,7 @@ import CategoryController from "../controllers/categoryControllers.js";
 
 const categoriesRoutes: Router = express.Router();
 
+categoriesRoutes.get("/v1/api/categories/:id", CategoryController.findById);
 categoriesRoutes.get("/v1/api/categories", CategoryController.findAllCategories);
 categoriesRoutes.post("/v1/api/categories", CategoryController.newCategory);
 
