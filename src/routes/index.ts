1163d7891093b18cc79dcd090144a452133a574c
Felipe Lohan <felipelohan03052006@gmail.com>
2025-05-24 10:21:26 -0300

    feat(routes): added user route to index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import express, { Application } from "express";
 import categoriesRoutes from "./categoryRoutes.js";
 import productsRoutes from "./productRoutes.js";
+import usersRoutes from "./userRoutes.js";
 
 const routes = (app: Application) => {
-  app.use(express.json(), categoriesRoutes, productsRoutes)
+  app.use(express.json(), categoriesRoutes, productsRoutes, usersRoutes)
 }
 
 export default routes;
