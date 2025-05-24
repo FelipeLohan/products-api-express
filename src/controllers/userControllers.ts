07a42e34d42da2444628935bc707951caa1ffe4f
Felipe Lohan <felipelohan03052006@gmail.com>
2025-05-24 10:20:02 -0300

    feat(controllers): added findAllUsers

---
+++ src/controllers/userControllers.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express";
+import user from "../models/User.js";
+import CustomError from "../utils/CustomError.js";
+
+class UserController {
+  static findAllUsers = async ( req: Request , res: Response ) => {
+    try {
+      const usersData = await user.find();
+      res.status(200).json({
+        "message": "success",
+        "status": 200,
+        "data": usersData
+      })
+    } catch (e) {
+      res.status(500).json({
+        "message": "fail",
+        "status": 500,
+        "description": `${e}`
+      })
+    }
+  }
+}
+
+export default UserController
